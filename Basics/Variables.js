7a8764f99a32bc7f69ce2bba69d9112b03c74316
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2024-03-04 21:12:19 +0530

    Variables

---
+++ Basics/Variables.js
@@ -0,0 +1,29 @@
+name = "Tony";
+age = 24;
+price = 99.99;
+x = null;
+y =undefined;
+isFollow = false;
+
+fullname = "Pradip Bhattacharjya";
+FULLNAME = "Subha Bhattacharjya";
+ 
+console.log(fullname);
+console.log(FULLNAME);
+
+
+//--------------------Let-------------------------
+
+let fullName = "tony stark";
+let Age = 24;
+let totalprice = 1000;
+let a;
+
+console.log(a);
+
+
+//-------------------------Const---------------------
+
+const PI =3.14;
+const A = 10;
+console.log(A);
