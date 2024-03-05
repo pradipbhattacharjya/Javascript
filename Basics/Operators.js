5a483ebccf2063412ea990740639415fc9880c04
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2024-03-05 21:09:38 +0530

    OPERATORS

---
+++ Basics/Operators.js
@@ -0,0 +1,58 @@
+//Artimetic Operators
+
+let a = 5;
+let b = 2;
+
+console.log("a = ", a, "& b =", b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+console.log("a % b =", a % b);
+console.log("a ** b =", a ** b);
+
+//Unary Operator
+console.log("a = ", a, "& b =", b);
+// a++;
+// a--;
+console.log("a=",a);
+
+//Assignment Operators
+
+//=, +=, -=, *=, %=, **=
+
+// let A = 5;
+// let B = 2;
+
+a += 4;//a= a+4
+console.log("a=",a);
+
+//Comparison Operators
+
+//==,!=, === ,!==, >, >=,< <=
+
+console.log("a == b" , a == b);
+console.log("a !== b" , a !== b);
+console.log("a != b" , a != b);
+console.log("a === b" , a === b);
+console.log("a > b" , a > b);
+console.log("a >= b" , a >= b);
+console.log("a < b" , a < b);
+console.log("a <= b" , a <= b);
+
+//LOgical Operators
+
+
+let A = 6;
+let B = 5;
+
+let cond1 = A > B;
+let cond2 = A === 6;
+console.log("cond1 && cond2", cond1 && cond2);
+console.log("cond1 || cond2 =", A < B || A === 6);
+console.log("!(6 < 5)=", !(A < b));
+
+
+
+
+
