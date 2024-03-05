a9b4df2f8455ea7d2b20978fc86f3659dfa584bc
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2024-03-05 21:58:13 +0530

    Qs2--> odd or even.

--- Practice/Qs4.js
+++ Practice/Qs4.js
@@ -1,7 +1,18 @@
-let age = 25;
+// let age = 25;
 
-if (age >=18) {
-    console.log("vote");
+// if (age >=18) {
+//     console.log("vote");
+// }else {
+//     console.log("not vote");
+// }
+
+//Qs2--> odd or even
+
+let num = 10;
+
+if(num%2 === 0) {
+    console.log(num,"even");
 }else {
-    console.log("not vote");
+    console.log(num,"odd");
 }
+
