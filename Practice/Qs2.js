a02915d33ab434fe753953ebbd3f45a7ff4da642
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2024-03-05 08:43:04 +0530

    Qs1Qs2

---
+++ Practice/Qs2.js
@@ -0,0 +1,13 @@
+//Create a const object called "profile" to store information shown in the picture.
+const profile = {
+    
+    fullName: "pradipbhattacharjya",
+    isFollow: true,
+    post: 195,
+    followers: 569,
+    following: 4,
+
+};
+
+console.log(profile);
+console.log(typeof profile);
