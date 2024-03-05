874a17c120dec4767473bc0ddb0e44c0a5fe4403
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2024-03-05 07:32:13 +0530

    Data Types

---
+++ Basics/DataTypes.js
@@ -0,0 +1,13 @@
+//Number,String,Boolean,Undefined,Null,Biglbt Symbol
+
+
+
+const student = {
+    fullName: "Pradip Bhattacharjya",
+    age:20,
+    cgpa:8.2,
+    isPass: true,
+};
+student["age"] = student ["age"] + 1;
+
+console.log(student.age);
