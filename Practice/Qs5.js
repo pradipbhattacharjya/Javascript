28170fe6fee3775d8a650d4796f7b5116e27fb77
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2024-03-06 08:23:42 +0530

    using prompt

---
+++ Practice/Qs5.js
@@ -0,0 +1,9 @@
+//get user to input a number using prompt("Enter a number:").Check if the number is a multiple of 5 or not.
+
+let num = prompt("enter a number:");
+
+if(num % 5 === 0) {
+    console.log(num,"is a multiple of 5");
+}else{
+    console.log(num, "is a NOT a multiple of 5");
+}
