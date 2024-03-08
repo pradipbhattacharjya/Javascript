f5a625389e62678b3a7916a18c0dd5be7b994861
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2024-03-08 17:50:13 +0530

    for-inloop

---
+++ LOOPS/for-in loop.js
@@ -0,0 +1,12 @@
+let student = {
+    name : "Rahul Kumar",
+    age : 20,
+    cgpa : 7.5,
+    ispass: true,
+
+};
+
+for (let key in student){
+    console.log("key=",key, "value=",student[key]);
+
+}
