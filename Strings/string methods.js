a3e51dab6fcd02d935339274f10f84b83f1013a7
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2024-03-09 19:04:34 +0530

    String Methods

---
+++ Strings/string methods.js
@@ -0,0 +1,26 @@
+let str = "pRaDiP";
+// let newStr= str.toUpperCase();
+
+
+let newStr = str.toLowerCase();
+console.log(str);
+console.log(newStr);
+
+
+let str2 ="     PradipBhattacharjya   ";
+
+console.log(str2.trim());
+
+let str3 ="bhattacharjya";
+console.log(str3.slice(1, 5));
+
+let res = str.concat(str3);
+console.log(res);
+
+let str4 = "hello";
+console.log(str4.replace("h","y"));
+
+let str5 ="Ilovejs"
+console.log(str5.charAt(3));
+
+
