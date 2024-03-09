8d8161d991f95cbf85015dfd5d1dae5d6386507f
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2024-03-09 08:38:42 +0530

    Template Literals

---
+++ Strings/Template Literals.js
@@ -0,0 +1,14 @@
+
+let obj = {
+    item: "pen",
+    price: 10,
+};
+
+let output =`this cost of ${obj.item} is ${obj.price} rupees`;
+console.log(output);
+// console.log("the cost of",obj.item,"is", obj.price, "rupees");
+
+//Template Literals
+let specialString = `This is a template literal`;
+console.log(specialString);
+console.log(typeof specialString)
---
+++ Strings/strigs.js
@@ -0,0 +1,8 @@
+let str = "Pradip";
+let str2 ='subha';
+
+console.log(str);
+console.log(str2);
+console.log(str2.length);
+console.log(str[4]);
+
