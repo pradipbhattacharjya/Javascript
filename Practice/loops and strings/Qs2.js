4e547926eaf3ae895d1557881cafbf71dff54b41
pradipbhattacharjya <pradipbhattacharjya1997@gmail.com>
2024-03-09 08:09:07 +0530

    loops qs

---
+++ Practice/loops and strings/Qs2.js
@@ -0,0 +1,12 @@
+//Create a game where you start with any random game number. Ask the user to keep guessing the game number until the user enters correct value.
+
+
+let gameNum = 25;
+
+let userNum = prompt ( "Guess the game number:");
+
+while(userNum != gameNum){
+
+}
+
+console.log("congratulations,you entered the right number");
