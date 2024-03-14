dbb99c0e63de4b4738171479122690b053595153
Pradip Bhattacharjya <93419916+pradipbhattacharjya@users.noreply.github.com>
2024-03-14 02:02:44 +0000

    Scope level and mini hosisting

--- Scope/scope.js
+++ Scope/scope.js
@@ -13,3 +13,46 @@ if(true){
     console.log("INNER:",a);
 }
 console.log(a);
+
+
+function one(){
+    const username = "pradip"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+one()
+
+if(true) {
+    const username = "PRADIP"
+    if(username === "PRADIP"){
+        const website = " youtube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
+
