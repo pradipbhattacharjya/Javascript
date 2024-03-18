a7c8e1190517f9a4d85f06ea50047ff0e052a8b3
Pradip Bhattacharjya <93419916+pradipbhattacharjya@users.noreply.github.com>
2024-03-18 16:07:55 +0000

    for in loop

--- Iterations/four.js
+++ Iterations/four.js
@@ -6,5 +6,17 @@ const myObject = {
 }
 
 for (const key in myObject) {
-    console.log(myObject[key]);
+    // console.log(myObject[key]);
+
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+// array in for in loop
+
+
+const programing = ["js", "rd", "py", "java","cpp"]
+
+for (const key in programing) {
+    
+    console.log(programing[key]);
 }
